f18f312576c592c7a2f8347af7609269568c430d
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-07-06 19:30:56 -0400

    Update gitpod chart

--- lib/gitpod.ts
+++ lib/gitpod.ts
@@ -47,7 +47,7 @@ export class GitpodStack extends cdk.Stack {
             release: 'gitpod',
             repository: 'https://aledbf.github.io/gitpod-chart-cleanup/',
             namespace: 'default',
-            version: '1.0.14',
+            version: '1.0.15',
             wait: true,
             values: loadYaml(doc),
         });
