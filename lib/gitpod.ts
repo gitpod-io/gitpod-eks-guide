6b6964b2e94c64a359423de641646a083ac897b2
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-07-06 23:26:44 -0400

    Update image tag

--- lib/gitpod.ts
+++ lib/gitpod.ts
@@ -7,7 +7,7 @@ import { Registry } from './registry';
 import { importCluster } from './charts/cluster-utils';
 
 // TODO: switch to official gitpod.io build
-const version = "aledbf-retag.9";
+const version = "aledbf-retag.10";
 
 export interface GitpodProps extends cdk.StackProps {
     domain: string
