aaa6751c9d03687c265d51de4fff38fe04c246ac
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-07-22 17:58:28 -0400

    Upgrade gitpod chart
    Signed-off-by: Manuel Alejandro de Brito Fontes <aledbf@gmail.com>

--- lib/gitpod.ts
+++ lib/gitpod.ts
@@ -6,8 +6,8 @@ import { Database } from './database';
 import { Registry } from './registry';
 import { importCluster } from './charts/cluster-utils';
 
-// TODO: switch to official gitpod.io build
-const version = "aledbf-retag.27";
+// TODO: switch to official gitpod.io build.
+const version = "aledbf-mk3.27";
 
 export interface GitpodProps extends cdk.StackProps {
     domain: string
@@ -47,7 +47,7 @@ export class GitpodStack extends cdk.Stack {
             release: 'gitpod',
             repository: 'https://aledbf.github.io/gitpod-chart-cleanup/',
             namespace: 'default',
-            version: '1.0.42',
+            version: '1.2.13',
             wait: true,
             values: loadYaml(doc),
         });
