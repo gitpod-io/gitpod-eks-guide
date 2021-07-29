2d2de774c144ee149f842abb68e898000e326f6d
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-07-29 11:41:40 -0400

    Update gitpod chart
    Signed-off-by: Manuel Alejandro de Brito Fontes <aledbf@gmail.com>

--- lib/gitpod.ts
+++ lib/gitpod.ts
@@ -18,7 +18,7 @@ var createNestedObject = function (base: any, names: any, value: any) {
 };
 
 // TODO: switch to official gitpod.io build.
-const version = "aledbf-mk3.40";
+const version = "aledbf-mk3.41";
 
 export interface GitpodProps extends cdk.StackProps {
     domain: string
